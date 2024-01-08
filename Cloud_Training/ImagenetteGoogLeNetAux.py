058b418418ee624123211680bdf06de6035994d3
rogerwang <rogerwang9@ucla.edu>
2024-01-08 13:42:50 -0800

    added GoogLeNet on Imagenette

---
+++ Cloud_Training/ImagenetteGoogLeNetAux.py
@@ -0,0 +1,43 @@
+# GoogLeNet with Auxillary classifiers on Imagenette
+
+from utils.Training import train
+from utils.Datasets import Imagenette
+from utils.ClassificationModel import GoogLeNet_Aux, initialize_he
+
+import torch
+from torchvision import transforms
+from torch.utils.data import DataLoader
+
+
+# loading data
+path = "../data/imagenette2-160"
+resize = (224,224)
+trans = transforms.Compose([transforms.Resize(resize),transforms.ToTensor()])
+trainset = Imagenette(True, root=path, transform=trans)
+valset = Imagenette(False, root=path, transform=trans)
+
+batch = 128
+trainloader = DataLoader(trainset,batch,True)
+valloader = DataLoader(valset,batch,False)
+
+# initialize model
+cuda = torch.device('cuda')
+model = GoogLeNet_Aux(10,lr=0.01,momentum=0.9,weight_decay=0.0002) # suggested from literature
+model.initialize_weights(next(iter(trainloader))[0],initialize_he)
+model.to(cuda)
+
+# training, reduce lr 3 times after val acc stops improving
+train(model,trainloader,valloader,epochs=50,delta=0.005)
+
+model.lr /= 2
+print(f'New LR: {model.lr}')
+train(model,trainloader,valloader,epochs=50,delta=0.003)
+
+model.lr /= 2
+print(f'New LR: {model.lr}')
+train(model,trainloader,valloader,epochs=50,delta=0)
+
+# checkpoint
+torch.save(model.state_dict(),'ImagenetteGoogLeNeAux.params')
+
+# val acc 0.813 after 47 epochs
